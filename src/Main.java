5648cb328da833fc39faebd2afd9994e21c0679a
Anna Gergert <annya.gergert@gmail.com>
2021-11-28 17:26:55 +0300

    Diners club

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "342482586140821";
+        String number = "30161858699733";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 

7f29db4f4c0c4488fc575744683f960fb1be1deb
Anna Gergert <annya.gergert@gmail.com>
2021-11-28 17:29:46 +0300

    JCB 15 digit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "214932552452050";
+        String number = "180077287369565";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 

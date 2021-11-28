b0986519fffb4e9e630974ecaa22d4e84d4c1b1b
Anna Gergert <annya.gergert@gmail.com>
2021-11-28 17:32:04 +0300

    Voyager

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4916694843692";
+        String number = "869983924664698";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
